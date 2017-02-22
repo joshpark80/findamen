f16036236059e711883b03c27d7a459ee7b9a07b
Hongmin Park <park.hongmin@gmail.com>
2017-02-23 00:03:35 +0900

    authorization for pin editing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222090842) do
+ActiveRecord::Schema.define(version: 20170222143310) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20170222090842) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
