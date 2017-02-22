196cf36f82f587d482f34e35e87f3e5b624d814c
Hongmin Park <park.hongmin@gmail.com>
2017-02-22 21:34:30 +0900

    signup feature added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Always update this name to real address 
+  config.action_mailer.default_url_options = { host: 'http://findamen.herokuapp.com/' }
+
 end
